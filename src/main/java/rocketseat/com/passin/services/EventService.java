35fcf74e6eaa7bc64f2b455145119826807465a0
davikrig-ha <davi.envolti@malwee.com.br>
2024-04-03 17:27:27 -0300

    init aula 3

--- src/main/java/rocketseat/com/passin/services/EventService.java
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -5,13 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.domain.event.exceptions.EventFullException;
 import rocketseat.com.passin.domain.event.exceptions.EventNotFoundException;
+import rocketseat.com.passin.dto.attendee.AttendeeRequestDTO;
 import rocketseat.com.passin.dto.event.EventIdDTO;
 import rocketseat.com.passin.dto.event.EventRequestDTO;
 import rocketseat.com.passin.dto.event.EventResponseDTO;
 import rocketseat.com.passin.repositories.EventRepository;
 
 import java.text.Normalizer;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -38,6 +41,22 @@ public class EventService {
         return new EventIdDTO(newEvent.getId());
     }
 
+    public void registerAttendeeOnEvent(String eventId, AttendeeRequestDTO attendeeRequestDTO){
+        this.attendeeService.verifyAttendeeSubscription(attendeeRequestDTO.email(), eventId);
+        Event event = this.eventRepository.findById(eventId).orElseThrow(() -> new EventNotFoundException("Event not found with ID:" + eventId));
+        List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
+
+        if(event.getMaximumAttendees() <= attendeeList.size()) throw new EventFullException("Event is full");
+
+        Attendee newAttendee = new Attendee();
+         newAttendee.setName(attendeeRequestDTO.name());
+         newAttendee.setName(attendeeRequestDTO.email());
+         newAttendee.setEvent(event);
+         newAttendee.setCreatedAt(LocalDateTime.now());
+
+         this.attendeeService.registerAttendee(newAttendee);
+    }
+
     private String createSlug(String text){
         String normalized = Normalizer.normalize(text, Normalizer.Form.NFD);
         return normalized.replaceAll("[\\p{InCOMBINING_DIACRITICAL_MARKS}]", "")
@@ -45,4 +64,5 @@ public class EventService {
                 .replaceAll("\\s+", "-")
                 .toLowerCase();
     }
+
 }
