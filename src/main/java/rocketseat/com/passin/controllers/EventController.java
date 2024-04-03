b1ad0ad02186ee16477290cbc78da706ec903473
davikrig-ha <davi.envolti@malwee.com.br>
2024-04-02 23:17:40 -0300

    aula 2

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -1,9 +1,41 @@
 package rocketseat.com.passin.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.passin.dto.attendee.AttendeesListResponseDTO;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.services.AttendeeService;
+import rocketseat.com.passin.services.EventService;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
+    private final EventService eventService;
+    private final AttendeeService attendeeService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id){
+        EventResponseDTO event = this.eventService.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    };
+
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder){
+       EventIdDTO eventIdDto = this.eventService.createEvent(body);
+
+       var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDto.eventId()).toUri();
+
+       return ResponseEntity.created(uri).body(eventIdDto);
+    }
+
+    @GetMapping("/attendees/{id}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendees(@PathVariable String id){
+        AttendeesListResponseDTO attendeesListResponse = this.attendeeService.getEventsAttendee(id);
+        return ResponseEntity.ok(attendeesListResponse);
+    };
 }
